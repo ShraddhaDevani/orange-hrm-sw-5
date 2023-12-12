6cb147e9bbadbb94d66553c405837a145e36512b
ShraddhaDevani <shraddhamorad@gmail.com>
2023-12-12 12:37:11 +0000

    submit

--- orange-hrm/src/test/java/resources/testdata/LoginTestData.java
+++ orange-hrm/src/test/java/resources/testdata/LoginTestData.java
@@ -8,12 +8,12 @@ public class LoginTestData extends BaseTest {
     @DataProvider(name = "dataset")
     public Object[][] getdata()
     {
-        Object [] [] data = new Object[][]
+        Object [][] data = new Object[][]
                 {
-                        { " " , " " , "Required"},
-                        { "test123@gmail.com" ," " ,"Required" },
-                        { " " , "test123" ,   "Required"},
-                        {"test123@gmail.com" ,"test123",  "Invalid credentials"}
+                        { " ", " ", "Required"},
+                        { "test123@gmail.com"," ","Required" },
+                        { " ", "test123" , "Required"},
+                        {"test123@gmail.com","test123", "Invalid credentials"}
 
                 };
         return data;
